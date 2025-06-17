a115d2f498b887beccc09f08c382c4eac6b735de
packarazy <105919980+packarazy@users.noreply.github.com>
2025-06-17 15:55:28 -0500

    Actualizar mode-latino.js

--- Ace/mode-latino.js
+++ Ace/mode-latino.js
@@ -1,1 +1,18 @@
+define(function(require, exports, module) {
+    "use strict";
 
+    var oop = require("../lib/oop");
+    var TextMode = require("./text").Mode;
+    var LatinoHighlightRules = require("./latino_highlight_rules").LatinoHighlightRules; // Cambiado aquí
+
+    var Mode = function() {
+        this.HighlightRules = LatinoHighlightRules;
+    };
+    oop.inherits(Mode, TextMode);
+
+    (function() {
+        this.$id = "ace/mode/latino"; // Cambiado aquí
+    }).call(Mode.prototype);
+
+    exports.Mode = Mode;
+});
